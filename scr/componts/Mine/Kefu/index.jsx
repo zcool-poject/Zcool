34f6406cdd23b014c32da83a5ff83ff02b42df76
WJD <973951780@qq.com>
2019-07-23 19:51:33 +0800

    提交mine

---
+++ scr/componts/Mine/Kefu/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Kefu.scss'
+
+
+function Kefu() {
+    return(
+        <div className="kefu">
+            <a href="javascript:;" className="user-item">
+                <span>客服中心</span>
+            </a>
+            <a href="javascript:;" className="kefu_itme">
+                <span>关于卷皮</span>
+            </a>
+        </div>
+    )
+}
+
+export default Kefu;

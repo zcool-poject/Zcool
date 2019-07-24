e4eecbc34e7cd72b1cea2b6f262dbed1125cc122
WJD <973951780@qq.com>
2019-07-24 09:42:45 +0800

    改src

---
+++ src/componts/login/index.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import './login/scss';
+
+class login extends Component() {
+    render(){
+        return(
+            <div id="heade">
+                <div className="fixtop">
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default login;
